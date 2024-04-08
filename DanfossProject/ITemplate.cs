bfe340186df3665ac406f94d27c15e8653544013
Adriana <adrianajaworska1717@gmail.com>
2024-04-08 19:45:37 +0200

    Model

---
+++ DanfossProject/ITemplate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanfossProject
+{
+    internal interface ITemplate
+    {
+        void ProvideAllConfigurationData(Model model);
+        void ProvideParameners();
+        
+        bool IsOperatingg();
+
+    }
+    internal interface interface2
+    {
+        //json???
+    }
+}
